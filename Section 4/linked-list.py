e16aba97ccea4ee9df4538e76623d839195daccc
Hong Than Brathna <hangbrathna10@gmail.com>
2025-01-17 15:52:55 +0700

    section 4: Linked List (Pop code)

--- Section 4/linked-list.py
+++ Section 4/linked-list.py
@@ -59,12 +59,49 @@ class LinkedList:
 # Otherwise, link the current tail node to the new node and update the tail.
 # Increment the length.
 
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail =  pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
+# Purpose: Removes and returns the last node from the list.
+# Logic: 
+# If the list is empty, return None.
+# Traverse the list to find the second-to-last node.
+# Update the tail to the second-to-last node.
+# Set the next pointer of the new tail to None.
+# Decrement the length.
+# If the list is empty, set head and tail to None.
+# Return the value of the removed node.
+
+
 my_linked_list = LinkedList(1)
-my_linked_list.append(3)
-my_linked_list.append(4)  
+my_linked_list.append(2)
+
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length, '\n')
 
 print('Linked List:')
 my_linked_list.print_list()
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop())
+
+# (1) Item - Returns 1 Node
+print(my_linked_list.pop())
+
+# (None) Item - Returns None (since the list is empty)
+print(my_linked_list.pop())
